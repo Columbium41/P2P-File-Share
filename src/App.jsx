f8a4b730d1870d67bb71de24182901bbaad8efb3
Charley Liu <charleyliu716@gmail.com>
2023-03-04 00:16:17 -0500

    added create button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-
+      <div className="create-room-container">
+        <button className="create-room-button">
+          <h2>Create Room</h2>
+        </button>
+      </div>
     </div>
   )
 }
