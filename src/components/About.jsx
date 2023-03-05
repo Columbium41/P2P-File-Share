832c16a04cb23e9701cd8c80d685fc3d80ee5de1
Charley Liu <charleyliu716@gmail.com>
2023-03-05 08:15:36 -0500

    Changed about and production links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,7 @@ function About() {
         <div className="about text-section">
             <h2>About</h2>
             <p>
-                WatchParty is a web application that allows users to host/join a room where people can
-                screenshare their screens. This web application was made during&nbsp;
+                FileShare is a web application that allows users to share files using WebRTC. This web application was made during&nbsp;
                 <a href="https://hackthehill.com/" target="_blank" className="text-link">Hack the Hill</a>, an annual
                 hackathon held at the University of Ottawa.
             </p>
